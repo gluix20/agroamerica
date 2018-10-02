2ae135e94cb39fe7fed2fc6cea9d716790c5c30a
Luis Galvez <lx.galvez@gmail.com>
2018-10-02 09:56:06 -0600

    PERIODOS CARGA reengineered

--- ADMIN/PERIODOS_CARGA_VW.sql
+++ ADMIN/PERIODOS_CARGA_VW.sql
@@ -3,9 +3,7 @@ select * from stg_periodos_carga_vw;
 create or replace view stg_periodos_carga_vw as 
 with periodos as (
 select periodo_cod, min(fecha) fecha, min(fecha_jde) fecha_jde 
-
 from agrostg.stg_fecha
-
 where fecha <= trunc(sysdate)
 and periodo_num != 14
 group by periodo_cod
@@ -14,16 +12,10 @@ order by 1 desc
 select 'FRO_OBI_PLANILLA' tipo, min(fecha) fecha, min(fecha_jde) fecha_jde from periodos
 where rownum <= 4
 union all
-select 'STG_PLANILLA_GT' tipo, to_date('01/01/2017','dd/mm/yyyy') fecha, 117001 fecha_jde from dual
-union all
-select 'STG_PLANILLA_EC' tipo, to_date('01/01/2017','dd/mm/yyyy') fecha, 117001 fecha_jde from dual
-union all
-select 'STG_MANO_OBRA' tipo, to_date('01/01/2017','dd/mm/yyyy') fecha, 117001 fecha_jde from dual
-union all
-select 'CUB_PLANILLA_GT_MV' tipo, to_date('01/01/2018','dd/mm/yyyy') fecha, 118001 fecha_jde from dual
-union all
-select 'STG_COSTOS_OG' tipo, to_date('01/01/2017','dd/mm/yyyy') fecha, 117001 fecha_jde from dual
-union all
-select 'STG_INVENTARIO_MV' tipo, to_date('01/01/2017','dd/mm/yyyy') fecha, 117001 fecha_jde from dual
+select tipo, fecha, fecha_jde from tab_periodos_carga
 ;
 
+select 'GENERICO-'||ano, min(fecha) fecha, min(fecha_jde) fecha_jde 
+from agrostg.stg_fecha
+group by ano
+order by 1 desc
